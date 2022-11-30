2b09108ac30c2ac2e55094a1df6a733a84b75c4f
jayesh <jayeshn2000@gmail.com>
2022-11-30 19:03:03 +0530

    first commit before the pipeline

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) =>{
+    res.send('<h1>Express demo app</h1>');
+})
+
+app.get('/products' , (req,res) => {
+    res.send('<h2> games </h2>');
+})
+   
+app.listen(port , ()=> {
+    console.log(`Demo app is listening to port: ${port}`);
+})
